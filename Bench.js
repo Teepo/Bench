f540a2f556a7455c4d48fccab3bfa48ffdfa6247
Anthony Ribeiro <anthony.ribeiro77@gmail.com>
2015-06-13 00:28:45 +0200

    BENCH: end report > display at the end all test report (sum & avg), and display the best & worst test.

--- Bench.js
+++ Bench.js
@@ -2,13 +2,25 @@ var Bench = {};
 
 Bench = function() {
 
+    var self = this;
+
     this.config = {
-        runtime : 1
+        iteration : 10
     };
 
     this.self = this;
     this.funcs = [];
-    this.reports = [];
+
+    this.executions = [];
+    this.report = [];
+
+    this.avg = [];
+
+    this.min_test = null;
+    this.min_value = null;
+
+    this.max_test = null;
+    this.max_value = null;
 
     this.add = function(func) {
         this.funcs.push(func);
@@ -16,11 +28,11 @@ Bench = function() {
 
     this.process = function(config) {
 
-        var config = extend({}, this.config, config);
+        this.config = extend({}, this.config, config);
 
         var i = 0;
 
-        while(i < config.runtime) {
+        while(i < this.config.iteration) {
 
             for (var x = 0; x < this.funcs.length; x++) {
 
@@ -30,31 +42,135 @@ Bench = function() {
 
                 var t1 = performance.now();
 
-                if (this.reports[x] == null)
-                    this.reports[x] = [];
-                if (this.reports[x][i] == null)
-                    this.reports[x][i] = [];
+                if (this.executions[x] == null)
+                    this.executions[x] = [];
+                if (this.executions[x][i] == null)
+                    this.executions[x][i] = [];
 
-                this.reports[x][i] = t1 - t0;
+                this.executions[x][i] = t1 - t0;
             }
 
             i++;
         }
 
-        console.log(this.reports);
+        this.report();
+    };
+
+    this.report = function() {
+
+        var str = [];
+        for (var x = 0; x < this.executions.length; x++)
+        {
+            var sum = 0;
+
+            // save best and worst test.
+            for (var i = 0; i < this.executions[x].length; i++)
+            {
+                var ms = this.executions[x][i];
+
+                // init min & max value
+                if (this.min_value == null)
+                {
+                    this.min_value = ms;
+                    this.min_test = x;
+                }
+                if (this.max_value == null)
+                {
+                    this.max_value = ms;
+                    this.max_test = x;
+                }
+
+                // search min & max value
+                if (ms < this.min_value)
+                {
+                    this.min_value = ms
+                    this.min_test = x;
+                }
+                if (ms > this.max_value)
+                {
+                    this.max_value = ms
+                    this.max_test = x;
+                }
+
+                if (this.avg[x] == null)
+                    this.avg[x] = 0;
+
+                this.avg[x] += ms;
+            }
+        }
+
+        delete i;
+        for (var i = 0; i < this.executions.length; i++)
+        {
+            var opt = null;
+            if (i == self.min_test)
+                opt = "color:#fff;background-color:rgb(40, 163, 40);";
+            if (i == self.max_test)
+                opt = "color:#fff;background-color:tomato;";
+
+            console.log(
+                "%cLe Test %d a mis en moyenne %f ms sur un panel de %f essais, pour un total de %f ms d'executions.",
+                opt, i, (this.avg[i] / this.config.iteration), this.config.iteration, this.avg[i]
+            );
+        };
     };
 };
 
-var test = new Bench();
+window.addEventListener('load', function() {
+    var test = new Bench();
 
-test.add(function() {
-        sleep(10);
-});
+    test.add(function() {
 
-test.add(function() {
-        sleep(20);
-});
+        var i = 0;
+        while(i <= 100)
+        {
+            $('body').html('<div class="lol"></div>');
+            $('body').html('');
 
-test.process({
-        runtime: 100
+            i++;
+        }
     });
+
+    test.add(function() {
+
+        var i = 0;
+
+        while(i <= 100)
+        {
+            var div = document.createElement('div');
+            div.classList.add('lol');
+
+            var body = document.getElementsByTagName('body')[0];
+
+            body.appendChild(div);
+
+            body.innerHTML = "";
+            i++;
+        }
+    });
+
+    test.add(function() {
+
+        var i = 0;
+        while(i <= 100)
+        {
+            $('<div></div>').addClass('lol').appendTo('body');
+            i++;
+        }
+    });
+
+    test.add(function() {
+
+        var i = 0;
+        while(i <= 100)
+        {
+            $('<div class="lol"></div>').appendTo('body');
+            i++;
+        }
+    });
+
+
+    test.process({
+        iteration: 234
+    });
+});
